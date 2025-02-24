966f6cd109141f83cdcef9537e251d903c6d5f34
payhez <payhez@gmail.com>
2025-02-24 14:06:20 +0100

    remove unnecessary import

--- src/main/java/com/crewmeister/cmcodingchallenge/CmCodingChallengeApplication.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/CmCodingChallengeApplication.java
@@ -3,7 +3,6 @@ package com.crewmeister.cmcodingchallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableScheduling
